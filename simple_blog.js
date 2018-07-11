0179f6fba927a3cf1e96e74826ae92a21ceaf899
Alex Chaffee <alex@stinky.com>
2018-07-11 07:37:31 -0400

    refactored and more robust

--- simple_blog.js
+++ simple_blog.js
@@ -11,6 +11,16 @@ http.createServer(function (request, response) {
   let data;
   let path = decodeURIComponent(request.url);
 
+  console.log('Finding ' + path);
+
+  function extractPathParameters(path) {
+    let pathParts = path.slice(1).split('/');
+    let action = pathParts.shift();
+    let id = pathParts.shift();
+    let pathParams = { action: action, id: id };
+    return pathParams;
+  }
+  
   function sendFile(file) {
     console.log('Sending ' + file);
     data = fs.readFileSync(file);
@@ -22,7 +32,6 @@ http.createServer(function (request, response) {
     data = message;
     response.statusCode = statusCode;
     contentType = 'text/plain';
-    file = null;
   }
 
   function sendDirectoryList(dir) {
@@ -33,60 +42,65 @@ http.createServer(function (request, response) {
     contentType = 'text/html';
   }
 
-
-
-  try {
-
-    // TODO: CLEAN UP CODE
-
-    // route "/articles/X" to article.html...
-    let pathParts = path.split('/')
-    console.log(pathParts);
-    pathParts.shift(); // get rid of ''
-    let action = pathParts.shift()
-    console.log(action)
-    if (action === 'articles') {
-      let articleFile;
-      let articleId = pathParts.shift();
-      if (articleId.endsWith('.json')) {
-        file = $path.join(publicDir, "articles", articleId);
-      } else {
-        articleFile = $path.join(publicDir, "articles", articleId + '.json');
-        console.log(articleFile)
-        if (fs.existsSync(articleFile)) {
-          file = $path.join(publicDir, "article.html");
-        } else {
-          sendError(404, `Article ${articleId} not found`);
+  function handleFileRequest() {
+    try {
+      file = $path.resolve($path.join(publicDir, path));
+      // verify that the requested file is in this directory, 
+      // not elsewhere on the server's filesystem
+      if (!file.startsWith(publicDir)) {
+        console.log("User requested file '" + request.url + "' (not permitted)");
+        sendError(403, "Error: you are not permitted to access that file."); // 403 Forbidden
+      }
+      else if (fs.statSync(file).isDirectory()) {
+        let indexFile = $path.join(file, "index.html");
+        if (fs.existsSync(indexFile)) {
+          sendFile(indexFile);
+        }
+        else {
+          sendDirectoryList(file);
         }
       }
-    }
-    else {
-      file = $path.resolve($path.join(publicDir, path));
-    }
-
-    if (!file) {
-      return;
-    }
-    // verify that the requested file is in this directory, 
-    // not elsewhere on the server's filesystem
-    if (!file.startsWith(publicDir)) {
-      console.log("User requested file '" + request.url + "' (not permitted)");
-      sendError(403, "Error: you are not permitted to access that file."); // 403 Forbidden
-    } else if (fs.statSync(file).isDirectory()) {
-      let indexFile = $path.join(file, "index.html");
-      if (fs.existsSync(indexFile)) {
-        sendFile(indexFile);
-      } else {
-        sendDirectoryList(file);
+      else {
+        sendFile(file);
       }
-    } else {
-      sendFile(file);
     }
-  } catch (error) {
-    console.log(error);
-    sendError(404, `Error: File ${path} not found`); // 404 Not Found
+    catch (error) {
+      console.log(error);
+      sendError(404, `File ${path} not found`); // 404 Not Found
+    }
+  }  
+
+  function handleArticleRequest(pathParams) {
+    let articleId = pathParams.id;
+    if (articleId.endsWith('.json')) {
+      // if it's asking for json, send it the raw file
+      file = $path.join(publicDir, "articles", articleId);
+      sendFile(file);
+    } else {
+      // if it's asking for HTML, send it the article.html file
+      articleFile = $path.join(publicDir, "articles", articleId + '.json');
+      if (fs.existsSync(articleFile)) {
+        file = $path.join(publicDir, "article.html");
+        sendFile(file);
+      } else {
+        sendError(404, `Article ${articleId} not found`);
+      }
+    }
   }
 
+  // extract path parameters from path
+  let pathParams = extractPathParameters(path);
+
+  if (pathParams.action === 'articles') {
+    if (pathParams.id) {
+      handleArticleRequest(pathParams);
+    } else {
+      data = "sorry, can't list articles yet"
+    }
+  }
+  else {
+    handleFileRequest();
+  }
 
   response.setHeader('Content-Type', contentType + '; charset=utf-8');
   response.write(data);
@@ -94,3 +108,4 @@ http.createServer(function (request, response) {
 }).listen(port);
 
 console.log("Listening on port " + port);
+
