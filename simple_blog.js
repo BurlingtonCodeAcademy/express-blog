49f6a784ffa1c7368dedae396052837677ff27e3
Alex Chaffee <alex@stinky.com>
2018-07-11 15:27:56 -0400

    refactor and improve blog server

--- simple_blog.js
+++ simple_blog.js
@@ -13,18 +13,36 @@ http.createServer(function (request, response) {
 
   console.log('Finding ' + path);
 
-  function extractPathParameters(path) {
+  function parsePath(path) {
+    let format;
+
+    if (path.endsWith('.json')) {
+      path = path.substring(0, path.length - 5);
+      format = 'json';
+    }
+
     let pathParts = path.slice(1).split('/');
     let action = pathParts.shift();
     let id = pathParts.shift();
-    let pathParams = { action: action, id: id };
+    let pathParams = { action: action, id: id, format: format };
     return pathParams;
   }
-  
+
   function sendFile(file) {
-    console.log('Sending ' + file);
-    data = fs.readFileSync(file);
-    contentType = mime.lookup(file);
+    try {
+      console.log('Sending ' + file);
+      data = fs.readFileSync(file);
+      contentType = mime.lookup(file);
+    }
+    catch (error) {
+      console.log(error);
+      if (error.code === 'ENOENT') {
+        let safeFileName = file.substring(publicDir.length);
+        sendError(404, `File ${safeFileName} not found`); // 404 Not Found
+      } else {
+        sendError(500, 'Unknown error'); // 404 Not Found
+      }
+    }
   }
 
   function sendError(statusCode, message) {
@@ -43,59 +61,61 @@ http.createServer(function (request, response) {
   }
 
   function handleFileRequest() {
-    try {
-      file = $path.resolve($path.join(publicDir, path));
-      // verify that the requested file is in this directory, 
-      // not elsewhere on the server's filesystem
-      if (!file.startsWith(publicDir)) {
-        console.log("User requested file '" + request.url + "' (not permitted)");
-        sendError(403, "Error: you are not permitted to access that file."); // 403 Forbidden
-      }
-      else if (fs.statSync(file).isDirectory()) {
-        let indexFile = $path.join(file, "index.html");
-        if (fs.existsSync(indexFile)) {
-          sendFile(indexFile);
-        }
-        else {
-          sendDirectoryList(file);
-        }
+    file = $path.resolve($path.join(publicDir, path));
+    // verify that the requested file is in this directory, 
+    // not elsewhere on the server's filesystem
+    if (!file.startsWith(publicDir)) {
+      console.log("User requested file '" + request.url + "' (not permitted)");
+      sendError(403, "Error: you are not permitted to access that file."); // 403 Forbidden
+    }
+    else if (fs.statSync(file).isDirectory()) {
+      let indexFile = $path.join(file, "index.html");
+      if (fs.existsSync(indexFile)) {
+        sendFile(indexFile);
       }
       else {
-        sendFile(file);
+        sendDirectoryList(file);
       }
     }
-    catch (error) {
-      console.log(error);
-      sendError(404, `File ${path} not found`); // 404 Not Found
-    }
-  }  
-
-  function handleArticleRequest(pathParams) {
-    let articleId = pathParams.id;
-    if (articleId.endsWith('.json')) {
-      // if it's asking for json, send it the raw file
-      file = $path.join(publicDir, "articles", articleId);
+    else {
       sendFile(file);
+    }
+  }
+
+  function sendArticle(pathParams) {
+    let articlesDir = $path.join(publicDir, "articles");
+    let articleDataFile = $path.join(articlesDir, pathParams.id + ".json");
+    if (pathParams.format === 'json') {
+      // if it's asking for json, send it the json file
+      sendFile(articleDataFile);
     } else {
       // if it's asking for HTML, send it the article.html file
-      articleFile = $path.join(publicDir, "articles", articleId + '.json');
-      if (fs.existsSync(articleFile)) {
-        file = $path.join(publicDir, "article.html");
-        sendFile(file);
+      if (fs.existsSync(articleDataFile)) {
+        let htmlFile = $path.join(publicDir, "article.html");
+        sendFile(htmlFile);
       } else {
         sendError(404, `Article ${articleId} not found`);
       }
     }
   }
 
-  // extract path parameters from path
-  let pathParams = extractPathParameters(path);
+  function sendArticleList() {
+    let articlesDir = $path.join(publicDir, "articles");
+    let files = fs.readdirSync(articlesDir)
+      .filter(file => file.endsWith('.json'))
+      .map(file => JSON.parse(fs.readFileSync($path.join(articlesDir, file))))
+    data = JSON.stringify(files);
+  }
 
+  let pathParams = parsePath(path);
+  console.log(pathParams)
   if (pathParams.action === 'articles') {
     if (pathParams.id) {
-      handleArticleRequest(pathParams);
+      sendArticle(pathParams);
+    } else if (pathParams.format === 'json') {
+      sendArticleList(data);
     } else {
-      data = "sorry, can't list articles yet"
+      sendFile($path.join(publicDir, 'articles.html'));
     }
   }
   else {
@@ -108,4 +128,5 @@ http.createServer(function (request, response) {
 }).listen(port);
 
 console.log("Listening on port " + port);
+
 
